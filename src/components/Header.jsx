e2228f7f350be20b31db1de16c078b622753c6a7
gabriel-shanahan <41175126+gshanahan@users.noreply.github.com>
2025-05-12 08:19:52 -0700

    Removed old title

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,6 @@
 export default function Header() {
   return (
     <header className="bg-blue-600 text-white p-4 shadow-md">
-      <h1 className="text-2xl font-semibold">TrueSquare</h1>
       <img src="https://gqshanahan.dev/images/TrueSquareLogo.png" alt="TrueSquare Logo" class="h-12 w-auto object-contain"/>    
   </header>
   );
