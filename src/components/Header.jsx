9bd49a9ed81cef020a105bbb80e1d1ed8f05a5dc
gabriel-shanahan <41175126+gshanahan@users.noreply.github.com>
2025-05-12 08:10:45 -0700

    Added TrueSquare Logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,7 @@ export default function Header() {
   return (
     <header className="bg-blue-600 text-white p-4 shadow-md">
       <h1 className="text-2xl font-semibold">TrueSquare</h1>
+      <img src="https://gqshanahan.dev/images/TrueSquareLogo.png" alt="TrueSquare Logo" class="true-square-logo"></img>
     </header>
   );
 }
