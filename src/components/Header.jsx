0fcbea1d4d1cbf1683d41e99bab0c1366330a59d
gabriel-shanahan <41175126+gshanahan@users.noreply.github.com>
2025-05-12 08:22:41 -0700

    Adjusted logo size

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
     <header className="bg-blue-600 text-white p-4 shadow-md">
-      <img src="https://gqshanahan.dev/images/TrueSquareLogo.png" alt="TrueSquare Logo" class="h-12 w-auto object-contain"/>    
+      <img src="https://gqshanahan.dev/images/TrueSquareLogo.png" alt="TrueSquare Logo" class="h-16 w-auto object-contain"/>    
   </header>
   );
 }
