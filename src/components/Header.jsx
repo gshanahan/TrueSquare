6a16e7bc4f9cffe1665c6dd10a7cf74f193f00d3
gabriel-shanahan <41175126+gshanahan@users.noreply.github.com>
2025-05-12 08:24:51 -0700

    Centered logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,13 @@
 export default function Header() {
   return (
     <header className="bg-blue-600 text-white p-4 shadow-md">
-      <img src="https://gqshanahan.dev/images/TrueSquareLogo.png" alt="TrueSquare Logo" class="h-16 w-auto object-contain"/>    
+    <div class="flex justify-center">
+      <img 
+        src="https://gqshanahan.dev/images/TrueSquareLogo.png" 
+        alt="TrueSquare Logo" 
+        class="h-16 w-auto object-contain"
+      />
+    </div>
   </header>
   );
 }
